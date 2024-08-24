5e6f9352e1fcf44f3d906d5ed7952a6cb37d8a2d
Eyobdd <eyobdavidoff@college.harvard.edu>
2024-08-24 03:12:39 -0700

    Hello Cargo!(1.3)

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-3939734a17fe0611.d
@@ -0,0 +1,5 @@
+/Users/eyobdavidoff/Documents/GitHub/RustTutorialRepo/projects/hello_cargo/target/debug/deps/hello_cargo-3939734a17fe0611: src/main.rs
+
+/Users/eyobdavidoff/Documents/GitHub/RustTutorialRepo/projects/hello_cargo/target/debug/deps/hello_cargo-3939734a17fe0611.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/eyobdavidoff/Documents/GitHub/RustTutorialRepo/projects/hello_cargo/target/debug/hello_cargo: /Users/eyobdavidoff/Documents/GitHub/RustTutorialRepo/projects/hello_cargo/src/main.rs
