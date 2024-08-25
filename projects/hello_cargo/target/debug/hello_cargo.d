2c4e43e917a96da9d25ca95e1611483e1af7f09e
Eyobdd <90155230+Eyobdd@users.noreply.github.com>
2024-08-25 16:18:40 -0700

    Delete projects/hello_cargo/target directory

--- projects/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/eyobdavidoff/Documents/GitHub/RustTutorialRepo/projects/hello_cargo/target/debug/hello_cargo: /Users/eyobdavidoff/Documents/GitHub/RustTutorialRepo/projects/hello_cargo/src/main.rs
